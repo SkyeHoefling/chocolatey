db225e8c94ff524126a2c99a0c8d64648af6b03b
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-03-17 19:45:49 -0400

    Initial powershell script (#2)
    * Initial add of the README file
    
    * add ps script, add vs2017 and choco
    
    * added basic dev tools
    
    * adding git to ps script

---
+++ installDevTools.ps1
@@ -0,0 +1,11 @@
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+choco install 7zip -y --force
+choco install notepadplusplus -y --force
+choco install fiddler4 -y --force
+choco install wireshark -y --force
+choco install paint.net -y --force
+choco install googlechrome -y --force
+choco install firefox -y --force
+choco install linqpad -y --force
+choco install git -y --force
