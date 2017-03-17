6a30eb37c00689d029553dd6dd5d47de3d35c135
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-03-17 19:33:39 -0400

    added basic dev tools

--- installDevTools.ps1
+++ installDevTools.ps1
@@ -1,3 +1,10 @@
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
-chocolatey install visualstudio2017professional -y --force
+choco install 7zip -y --force
+choco install notepadplusplus -y --force
+choco install fiddler4 -y --force
+choco install wireshark -y --force
+choco install paint.net -y --force
+choco install googlechrome -y --force
+choco install firefox -y --force
+choco install linqpad -y --force
